cee52f43fc0a425f9947cde0981a59bb45775056
Yasuhiro Asaka <yasaka@ywesee.com>
2012-04-10 16:10:30 +0200

    Added cleanup method

--- lib/migel/plugin/swissindex.rb
+++ lib/migel/plugin/swissindex.rb
@@ -31,44 +31,49 @@ module Migel
       total      = @migel_codes.length
       CSV.open(file_name, 'w') do |writer|
         SWISSINDEX_NONPHARMA_SERVER.session(ODDB::Swissindex::SwissindexNonpharma) do |swissindex|
-          @migel_codes.each_with_index do |migel_code, count|
-            product_flag = false
-            if migelid = get_migelid_by_migel_code(migel_code)
-              migel_code = migelid.migel_code.split('.').join
-              table = swissindex.search_migel_table(migel_code, 'MiGelCode', lang)
-              unless table.empty?
-                products = table.select{ |record| record[:pharmacode] and record[:article_name] }
-                products.each do |record|
-                  writer << [
-                    migel_code,
-                    record[:pharmacode],
-                    record[:ean_code],
-                    record[:article_name],
-                    record[:companyname],
-                    record[:companyean],
-                    record[:ppha],
-                    record[:ppub],
-                    record[:factor],
-                    record[:pzr],
-                    record[:size],
-                    record[:status],
-                    record[:datetime],
-                    record[:stdate],
-                    record[:language],
-                  ]
-                  saved_products += 1
-                  product_flag = true
+          if swissindex.download_all
+            @migel_codes.each_with_index do |migel_code, count|
+              product_flag = false
+              if migelid = get_migelid_by_migel_code(migel_code)
+                migel_code = migelid.migel_code.split('.').join
+                table = swissindex.search_migel_table(migel_code, 'MiGelCode', lang)
+                unless table.empty?
+                  products = table.select{ |record| record[:pharmacode] and record[:article_name] }
+                  products.each do |record|
+                    writer << [
+                      migel_code,
+                      record[:pharmacode],
+                      record[:ean_code],
+                      record[:article_name],
+                      record[:companyname],
+                      record[:companyean],
+                      record[:ppha],
+                      record[:ppub],
+                      record[:factor],
+                      record[:pzr],
+                      record[:size],
+                      record[:status],
+                      record[:datetime],
+                      record[:stdate],
+                      record[:language],
+                    ]
+                    saved_products += 1
+                    product_flag = true
+                  end
                 end
               end
+              if product_flag
+                migel_codes_with_products << migel_code
+              else
+                migel_codes_without_products << migel_code
+              end
+              time = estimate_time(start_time, total, count + 1) 
+              puts time if estimate
             end
-            if product_flag
-              migel_codes_with_products << migel_code
-            else
-              migel_codes_without_products << migel_code
-            end
-            time = estimate_time(start_time, total, count + 1) 
-            puts time if estimate
+          else
+            # pass
           end
+          swissindex.cleanup_items
         end # SWISSINDEX_NONPHARMA_SERVER
       end # CSV.open
       return [
