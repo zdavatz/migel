a9c19e3a493d459cd16f670590f91be0cb6dc235
Niklaus Giger <niklaus.giger@member.fsf.org>
2016-08-15 10:39:06 +0200

    Inactive Bauerfeind products before import via CSV

--- lib/migel/util/csv_importer.rb
+++ lib/migel/util/csv_importer.rb
@@ -27,6 +27,8 @@ module Migel
         @nr_updated = 0
         @nr_ignored = 0
         @nr_records = 0
+        @nr_products_before = 0
+        @nr_products_after = 0
         @migel_codes_with_products = []
         @migel_codes_without_products = []
       end
@@ -37,6 +39,7 @@ module Migel
         @update_time = (end_time / 60.0).to_i
         res = [
           "Total time to update: #{"%.2f" % @update_time} [m]",
+          sprintf("found via %s or %s were. Active products before/now: %d/%d", Companyname_DE, Companyname_FR, @nr_products_before-@nr_products_after, get_nr_active_bauerfeind_products),
           "Read CSV-file: #{@csv_file}",
           sprintf("Total %5i Migelids (%5i Migelids have products / %5i Migelids have no products)",
                   @nr_records,
@@ -64,40 +67,46 @@ module Migel
 
       def import_all_products_from_csv(options)
         @start_time = Time.now
-        puts "import_all_products_from_csv: options are #{options}"
+        puts "#{Time.now}: import_all_products_from_csv: options are #{options}"
         file_name = options[:filename]
-        puts "import_all_products_file_name are #{file_name.inspect}"
+        puts "#{Time.now}: import_all_products_file_name are #{file_name.inspect}"
         file_name = '/var/www/migel/data/csv/update_migel_bauerfeind.csv' unless file_name && file_name.length > 0
         unless File.exist?(file_name)
-          puts "Unable to open #{file_name}"
+          puts "#{Time.now}: Unable to open #{file_name}"
           return false
         end
         lang = (options[:lang] ||= 'de')
         estimate = (options[:estimate] || false)
-        puts "import_all_products_from_csv: file_name #{file_name} lang #{lang} estimate #{estimate}"
+        puts "#{Time.now}: import_all_products_from_csv: file_name #{file_name} lang #{lang} estimate #{estimate}"
         @csv_file = File.expand_path(file_name)
         @data_dir = File.dirname(@csv_file)
         FileUtils.mkdir_p @data_dir
         lang.upcase!
         total = File.readlines(file_name).to_a.length
         count = 0
+        @nr_products_before = get_nr_active_bauerfeind_products
+        set_bauerfeind_products_inactive
         products = Migel::Util::Server.new.all_products
         CSV.foreach(file_name, :col_sep => ';') do |line|
           count += 1
           migel_code = line[5]
           next if /Migel/i.match(migel_code)
           if line[4] == nil || line[4].length == 0
-            puts "Missing pharmacode in line #{count}: #{line}"
+            puts "#{Time.now}: Missing pharmacode in line #{count}: #{line}"
             next
           end
           @nr_records += 1
+          ean13 = line[1]
           if migelid = Migel::Model::Migelid.find_by_migel_code(migel_code)
-            ean13 = line[1]
             pharmacode = line[4]
-            # with_matching_ean = products.find_all{ |x,y| y.migel_code.eql?(migel_code) && y.ean_code.eql?(ean13)}
+            nr_invalids = migelid.products.find_all{|i| i.pharmacode.to_i == 0}.size
+            if nr_invalids > 0
+              puts "#{Time.now}: Deactivating non digital pharmacode #{pharmacode} for #{migel_code} found via #{line}"
+              migelid.products.delete_if{|i| i.pharmacode.to_i == 0 }
+              migelid.save
+            end
             with_matching_ean = migelid.products.find_all{|i| i.ean_code == ean13}
-            with_matching_pharmacode = migelid.products.find_all{|i| i.pharmacode == pharmacode}
-            # with_matching_pharmacode = products.find_all{ |x,y| y.migel_code.eql?(migel_code) && y.pharmacode.eql?(pharmacode)}
+            with_matching_pharmacode = migelid.products.find_all{|i| i.pharmacode.to_i != 0 && i.pharmacode == pharmacode}
             record = {
               :ean_code     => ean13,
               :pharmacode   => pharmacode,
@@ -105,35 +114,63 @@ module Migel
               :article_name_de => line[7].gsub(/,([^\s])/, ", \\1"),
               :article_name_fr => line[8].gsub(/,([^\s])/, ", \\1"),
             }
-            #require 'pry'; binding.pry
-            # puts "Short/long do not match in line #{count}: #{line}" unless line[3].eql?(line[8]) && line[2].eql?(line[7])
+            # puts "#{Time.now}: Short/long do not match in line #{count}: #{line}" unless line[3].eql?(line[8]) && line[2].eql?(line[7])
 
             @migel_codes_with_products << migel_code
             if with_matching_pharmacode.size == 1
               update_product_from_csv(migelid, record)
-              puts "updating: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code}" if estimate
-            elsif with_matching_ean.size == 1
+              puts "#{Time.now}: updating via_pharmacode: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code}" if estimate
+            elsif with_matching_ean.size >= 1
               update_product_from_csv(migelid, record)
-              puts "updating: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code} ean13 #{ean13}" if estimate
+              puts "#{Time.now}: updating via_ean: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code} ean13 #{ean13}" if estimate
             elsif with_matching_ean.size == 0 && with_matching_pharmacode.size == 0
               update_product_from_csv(migelid, record)
-              puts "Added as no matching ean/pharmacode found: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code} #{ean13}/#{pharmacode}" if estimate
+              puts "#{Time.now}: Added as no matching ean/pharmacode found: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code} #{ean13}/#{pharmacode}" if estimate
+            else
+              puts "#{Time.now}: Skipping : " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code} #{ean13}/#{pharmacode}" if estimate
             end
           else
             @migel_codes_without_products << migel_code
-            puts "ignoring as no code found: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code}" if estimate
+            puts "#{Time.now}: ignoring as no code found: " + estimate_time(@start_time, total, count, ' ') + "migel_code: #{migel_code} ean13 #{ean13}" if estimate
           end
         end
-        puts "finished: count #{count}: @nr_records #{@nr_records} " +
-            "@migel_codes_without_products #{@migel_codes_without_products.size} @migel_codes_with_products #{@migel_codes_with_products}"
+        clear_inactive_bauerfeind_products
+        @nr_products_after = get_nr_active_bauerfeind_products
+        puts "#{Time.now}: finished: count #{count}: @nr_records #{@nr_records} " +
+            "@migel_codes_without_products #{@migel_codes_without_products.size} @migel_codes_with_products #{@migel_codes_with_products.uniq}"
         true
       end
       private
+      def get_nr_active_bauerfeind_products
+        Migel::Model::Product.all.find_all{|x| x.status == 'A' && /bauerfeind/i.match(x.companyname.to_s)}.size
+      end
+      def clear_inactive_bauerfeind_products
+        items = 0
+        Migel::Model::Migelid.all.each do |migel_id|
+            migel_code = migel_id.migel_code
+            nr_invalids = migel_id.products.find_all{|x| x.status == 'I' && /Bauerfeind/i.match(x.companyname.to_s) }.size
+            if nr_invalids > 0
+              items += nr_invalids
+              puts "#{Time.now}: Total #{items}: Deleting  #{nr_invalids} items for #{migel_code} by Bauerfeind"
+              migel_id.products.delete_if{|x| x.status == 'I' && /Bauerfeind/i.match(x.companyname.to_s)  }
+              migel_id.save
+            end
+        end
+        puts "#{Time.now}: Deleted #{items} inactive Bauerfeind items"
+      end
+     def set_bauerfeind_products_inactive
+       items = 0
+       Migel::Model::Product.all.find_all{|x| x.status == 'A' && /bauerfeind/i.match(x.companyname.to_s)}.each{|x| x.status = 'I'; x.save; items += 1 }
+       puts "#{Time.now}: Set #{items} Bauerfeind items to inactive"
+      end
       def update_product_from_csv(migelid, record)
-        product = migelid.products.find{|i| i.pharmacode == record[:pharmacode]} || begin
+        found = true
+        key_value = record[:pharmacode].to_i != 0 ? record[:pharmacode] : record[:ean_code]
+        product = migelid.products.find do |i| i.pharmacode.eql?(key_value) end || begin
+          found = false
           orig_size = migelid.products.size
           origs_size2 = Migel::Util::Server.new.all_products.size
-          i = Migel::Model::Product.new(record[:pharmacode])
+          i = Migel::Model::Product.new(key_value)
           sleep 0.01
           migelid.products.push i
           sleep 0.01
@@ -141,6 +178,7 @@ module Migel
           i
         end
         product.migelid = migelid
+        product.pharmacode = key_value unless product.pharmacode.eql?(key_value)
         product.ean_code = record[:ean_code]
         product.send(:companyname).send('de=', Companyname_DE)
         product.send(:companyname).send('fr=', Companyname_FR)
--- spec/util/csv_importer_spec.rb
+++ spec/util/csv_importer_spec.rb
@@ -28,6 +28,7 @@ module Migel
       before(:each) do
         set_test_mail
         @importer = Migel::Util::CsvImporter.new
+        expect(Migel::Model::Product).to receive(:all).with(no_args).and_return( [ ]).at_least(1)
       end
       it "import_all_products_from_csv must return false" do
         file_to_import = File.expand_path(File.join(File.dirname(__FILE__), '..', 'data', 'should_not_exist.csv'))
@@ -45,6 +46,7 @@ module Migel
       before(:each) do
         set_test_mail
         @importer = Migel::Util::CsvImporter.new
+        expect(Migel::Model::Product).to receive(:all).with(no_args).and_return( [ ]).at_least(1)
       end
       it "import_all_products_from_csv must return false" do
         options = {
@@ -63,14 +65,17 @@ module Migel
     end
     MigelTestCode = '05.02.03.00.1'
     VenoTrainCode = '17.01.02.00.1'
-
+    GenuTrainCode = '05.04.04.00.1'
     describe CsvImporter, "Examples" do
       before(:each) do
         set_test_mail
         allow(ODBA.cache).to receive(:index_keys).and_return(['migel_code'])
         multilingual = double('multilingual')
-        expect(multilingual).to receive(:de=).with('Bauerfeind AG').and_return(nil)
-        expect(multilingual).to receive(:fr=).with('Bauerfeind SA').and_return(nil)
+        multilingual_company = double('multilingual_company')
+        expect(multilingual_company).to receive(:de=).with('Bauerfeind AG').and_return(nil)
+        expect(multilingual_company).to receive(:fr=).with('Bauerfeind SA').and_return(nil)
+
+        multilingual = double('multilingual')
         expect(multilingual).to receive(:de=).with('AchilloTrain, titan, rechts, 1').and_return(nil)
         expect(multilingual).to receive(:fr=).with('AchilloTrain, titane, droit, 1').and_return(nil)
         expect(multilingual).to receive(:save).with(no_args).and_return(nil).never
@@ -101,16 +106,32 @@ module Migel
         expect(product_2244350).to receive(:migelid=).and_return(nil)
         expect(product_2244350).to receive(:migelid).and_return(migelid).never
         expect(product_2244350).to receive(:ppub=).with('120.50').and_return(nil)
-        expect(product_2244350).to receive(:save).with(no_args).and_return(nil)
+        expect(product_2244350).to receive(:save).with(no_args).and_return(nil).at_least(:once)
         expect(product_2244350).to receive(:ean_code).with(no_args).and_return('4046445108532').at_least(:once)
         expect(product_2244350).to receive(:ean_code=).with("4046445108009").and_return(nil)
         expect(product_2244350).to receive(:ean_code=).with("4046445108532").and_return(nil).never
-        expect(product_2244350).to receive(:status=).with("A").and_return(nil)
-        expect(product_2244350).to receive(:companyname).and_return(multilingual).twice
+        @status_2244350 = nil
+        expect(product_2244350).to receive(:status=).with("A").and_return(@status_2244350 = 'A')
+        expect(product_2244350).to receive(:status).and_return('A').at_least(1)
+        expect(product_2244350).to receive(:companyname).with(no_args).and_return(multilingual_company).at_least(1)
+        @status_with_ean = nil
+        expect(product_with_ean).to receive(:companyname).with(no_args).and_return(multilingual_company).at_least(1)
+        expect(product_with_ean).to receive(:status).and_return('A')
+        expect(product_with_ean).to receive(:status).and_return(@status_with_ean).at_least(1)
         @server = Migel::Util::Server.new
         allow_any_instance_of(DRbObject).to receive(:session).and_return(@server)
         allow(ODBA.cache).to receive(:fetch_named).with('all_products', any_args).and_return( { MigelTestCode => product_2244350, VenoTrainCode => product_with_ean})
+        expect(Migel::Model::Product).to receive(:all).with(no_args).and_return( [ product_2244350, product_with_ean]).at_least(1)
+        expect(Migel::Model::Migelid).to receive(:all).with(no_args).and_return([migelid]).once
         expect(Migel::Model::Migelid).to receive(:find_by_migel_code).with('9999.99.99.99').and_return(nil).once
+        migelid_genutrain = double('migelid_genutrain',
+                         :migel_code => GenuTrainCode,
+                         :delete => true,
+                         :products => [],
+                         :add_product => nil,
+                         :save => nil,
+                        )
+        expect(Migel::Model::Migelid).to receive(:find_by_migel_code).with(GenuTrainCode).and_return(migelid_genutrain).at_least(1)
         expect(Migel::Model::Migelid).to receive(:find_by_migel_code).with(MigelTestCode).and_return(migelid).twice
         expect(Migel::Model::Migelid).to receive(:find_by_migel_code).with(VenoTrainCode).and_return(migelid).once
         group = double('group',
@@ -126,9 +147,10 @@ module Migel
         expect(@importer.import_all_products_from_csv({:filename => file_to_import})).to eq(true)
         report = @importer.report
         expect(report.find{|line| /Total time to update/.match(line)}).not_to eq(nil)
-        success = 'Total     4 Migelids (    3 Migelids have products /     1 Migelids have no products)'
+        success = 'Total     6 Migelids (    5 Migelids have products /     1 Migelids have no products)'
         expect(report.find{|line| success.eql?(line)}).to eq(success)
-        expected = 'Migelids with products (3)'
+        expect(report.find_all{|line| /found via Bauerfeind AG or Bauerfeind SA were. Active products before/.match(line)}.size).to eq 1
+        expected = 'Migelids with products (5)'
         expect(report.find{|line| expected.eql?(line)}).to eq(expected)
         expect(::Mail::TestMailer.deliveries.size).to eq(1)
         expect(::Mail::TestMailer.deliveries.first.to_s).not_to match /RuntimeError/
